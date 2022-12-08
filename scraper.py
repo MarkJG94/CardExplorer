b4ee31da57c9b698c1c6302db8fef7d3c0bb224d
MarkJG94 <mark.gallacher94@gmail.com>
2022-12-08 23:33:11 +0000

    Added error handling to scraper if it finds any css tags that do not meet conditions

--- scraper.py
+++ scraper.py
@@ -22,22 +22,21 @@ def parse_products(html):
     for item in products:
         new_item = Card(
         cardname = None,
-        price = item.css_first("span.price").text().strip()
-    )
+        price = None
+        )
+        try:
+            new_item.cardname = item.css_first("h4.name").text()
+            new_item.price = item.css_first("span.price").text().strip()
+        except:
+            pass
+        if (new_item.cardname is not None and new_item.price is not None):
+            print(asdict(new_item))
 
-    try:
-        new_item.cardname = item.css_first("h4.name").text()
-    except:
-        print("Error: Could not find element with CSS tag 'h4.name'.")
-
-    print(asdict(new_item))
 def main():
-    for x in range (1):
-        html = get_html(x)
-        print(html.css_first("title").text())
-        parse_products(html)
-        print("test")
-
+    html = get_html(1)
+    print(html.css_first("title").text())
+    parse_products(html)
+        
 if __name__ == '__main__':
     main()
 
