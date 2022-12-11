3fb9c150acbfc14613a62bbcdcde99b58e01416c
MarkJG94 <mark.gallacher94@gmail.com>
2022-12-11 21:44:10 +0000

    Can now search multiple stores as set up in scraper.py

---
+++ websites.py
@@ -0,0 +1,8 @@
+class TrollTraderCards:
+    def __init__(self):
+        self.url = "https://www.trolltradercards.com/products/search?q="
+
+class AxionNow:
+    def __init__(self):
+        self.url = "https://www.axionnow.com/products/search?q="
+
