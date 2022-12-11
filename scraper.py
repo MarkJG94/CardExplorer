b358cd7c29c86cb4a2b8fee7d8601bdee982fece
MarkJG94 <mark.gallacher94@gmail.com>
2022-12-11 23:06:48 +0000

    Automatic parsing of all objects in websites.py

--- scraper.py
+++ scraper.py
@@ -1,42 +1,53 @@
 import httpx
-from trolltrader import TrollTraderCards
-from axionnow import AxionNow
+from websites import *
 from selectolax.parser import HTMLParser
 from dataclasses import dataclass, asdict
+from urllib.parse import quote
 
 @dataclass
 class Card:
     cardname: str
     price: float
-  
-def get_html(page):
-    website = AxionNow()
-    search = input("Enter card name you wish to search: ")
-    searchurl = website.url + search
+
+
+def get_html(card, store, page):
+    search = card.replace(" ", store.whitespace)
+    searchurl = store.url + search
     resp = httpx.get(searchurl)
     return HTMLParser(resp.text)
 
-def parse_products(html):
-    products = html.css("div.meta")
-    results = []
+def load_stores_as_array():
+    stores = []
+    stores.append(TrollTraderCards())
+    stores.append(AxionNow())
+    stores.append(MagicMadhouse())
+    return stores
+
+def parse_products(card, store, html):
+    products = html.css(store.productsCSS)
     for item in products:
         new_item = Card(
         cardname = None,
         price = None
         )
         try:
-            new_item.cardname = item.css_first("h4.name").text()
-            new_item.price = item.css_first("span.price").text().strip()
+            new_item.cardname = item.css_first(store.cardnameCSS).text()
+            new_item.price = item.css_first(store.priceCSS).text().strip()
         except:
             pass
         if (new_item.cardname is not None and new_item.price is not None):
-            print(asdict(new_item))
+            if new_item.cardname in card:
+                print(asdict(new_item))
 
 def main():
-    html = get_html(1)
-    print(html.css_first("title").text())
-    parse_products(html)
+    stores = load_stores_as_array()
+    initsearch = input("Enter card name you wish to search: ")
+    CARD_TO_SEARCH = initsearch
+    for store in stores:
+        html = get_html(CARD_TO_SEARCH, store, 1)
+        print(html.css_first("title").text())
+        parse_products(CARD_TO_SEARCH, store, html)
+    
         
 if __name__ == '__main__':
     main()
-
--- websites.py
+++ websites.py
@@ -1,8 +1,23 @@
 class TrollTraderCards:
     def __init__(self):
         self.url = "https://www.trolltradercards.com/products/search?q="
+        self.whitespace = "+"
+        self.productsCSS = "div.meta"
+        self.cardnameCSS = "h4.name"
+        self.priceCSS = "span.price"
 
 class AxionNow:
     def __init__(self):
         self.url = "https://www.axionnow.com/products/search?q="
+        self.whitespace = "+"
+        self.productsCSS = "div.meta"
+        self.cardnameCSS = "h4.name"
+        self.priceCSS = "span.price"
 
+class MagicMadhouse:
+    def __init__(self):
+        self.url = "https://magicmadhouse.co.uk/search.php?search_query="
+        self.whitespace = "%20"
+        self.productsCSS = "#kuLandingProductsListUl"
+        self.cardnameCSS = "#kuLandingProductsListUl > li:nth-child(1) > div.kuNameDesc > div.kuName > a"
+        self.priceCSS = "#kuLandingProductsListUl > li:nth-child(1) > div.kuPrice > div"
